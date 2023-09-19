88b8994402e1564aacc6a5f6eb141d02bde354eb
Brendan Bobryk <babobryk@gmail.com>
2023-09-19 01:11:12 -0700

    Added comments

--- src/helloworld/src/main/java/com/cis084javaprogramming/App.java
+++ src/helloworld/src/main/java/com/cis084javaprogramming/App.java
@@ -1,13 +1,15 @@
 package com.cis084javaprogramming;
 
 /**
- * Hello world!
- *
+ * Name: Brendan Bobryk
+ * Student ID #: 1036738
+ * Class: CIS 084 Java Programming
+ * Date: 09/19/23
+ * Program: Primitive Data Types
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
